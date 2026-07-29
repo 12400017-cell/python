37768284b6e80178785229eeeae36a2c4a47de76
12400017-cell <12400017@aluno.cotemig.com.br>
2026-07-29 16:06:26 -0300

    sequencia1

--- atv/Atv4_5_menuLateral/app.py
+++ atv/Atv4_5_menuLateral/app.py
@@ -9,5 +9,13 @@ app = Flask(__name__)
 def homeP():
     return render_template("home.html")
 
+@app.route("/page1")
+def  pag1():
+    return render_template("page1.html")
+
+@app.route("/page2")
+def  pag2():
+    return render_template("page2.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
